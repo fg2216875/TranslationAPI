c3e1ae12596214423999207f7691dd0ffa196172
Ayao <fg2216875@gmail.com>
2025-03-19 14:04:34 +0800

    設定NLog.config檔案

--- TranslationAPI/Controller/TranslationController.cs
+++ TranslationAPI/Controller/TranslationController.cs
@@ -8,6 +8,8 @@ using System.Text.RegularExpressions;
 using TranslationAPI.Interface;
 using TranslationAPI.Model;
 using WebApplication2.Service;
+using Microsoft.Extensions.Logging;
+using NLog;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,13 +19,7 @@ namespace WebApplication2.Controller
     [ApiController]
     public class TranslationController : ControllerBase
     {
-        // GET: api/<ValuesController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
         private readonly IGeminiTranslationService _translationService;
 
         public TranslationController(IGeminiTranslationService translationService)
@@ -31,58 +27,73 @@ namespace WebApplication2.Controller
             _translationService = translationService;
         }
 
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
         [HttpPost]
         [Route("translate")]
         public async Task<ActionResult<TranslationResponse>> Translate([FromBody] HtmlContentRequest TextNodes)
         {
-            if (TextNodes == null)
+            try
             {
-                return BadRequest("HtmlContent and TextContent cannot be null or empty.");
-            }
-            string englishText = "";
-            foreach (var textNode in TextNodes.HTMLTextNodes.textNodes)
-            {
-                foreach (var kvp in textNode)
+                if (TextNodes == null)
                 {
-                    // 處理每個節點的數據，合併成一個字串後再呼叫API進行翻譯
-                    englishText += kvp.Key + ": " + kvp.Value + "|"; 
+                    return BadRequest("HtmlContent and TextContent cannot be null or empty.");
                 }
+                string englishText = "";
+                foreach (var textNode in TextNodes.HTMLTextNodes.textNodes)
+                {
+                    foreach (var kvp in textNode)
+                    {
+                        // 處理每個節點的數據，合併成一個字串後再呼叫API進行翻譯
+                        englishText += kvp.Key + ": " + kvp.Value + "|"; 
+                    }
+                }
+                // 將字串依據 '|' 符號分割
+                string[] parts = englishText.TrimEnd('|').Split('|');
+                // 計算每部分應該包含多少項目
+                //var perPart = Math.Ceiling((decimal)parts.Length / 15);
+                //List<string[]> engTextResult = new List<string[]>();
+                //for (int i = 0; i < perPart; i++)
+                //{
+                //    string[] chunk = parts.Skip(i * 15).Take(15).ToArray();
+                //    engTextResult.Add(chunk);
+                //}
+
+                //Gemini API免費版每分鐘可接收request次數為15次
+                int perPart = (int)Math.Ceiling(parts.Length / 15.0);
+                List<string[]> engTextResult = new List<string[]>();
+                for (int i = 0; i < 15; i++)
+                {
+                    string[] chunk = parts.Skip(i * perPart).Take(perPart).ToArray();
+                    engTextResult.Add(chunk);
+                }
+
+                //var tasks = engTextResult.Select(engTexts => _translationService.TranslateToChineseLocalAsync(engTexts)).ToList();
+                //await Task.WhenAll(tasks);
+                var tasks = engTextResult.Select(engTexts => _translationService.TranslateToChineseAsync(engTexts)).ToList();
+                await Task.WhenAll(tasks);
+
+                string chineseText = "";
+                for (int i = 0; i < tasks.Count; i++) {
+                    chineseText += tasks[i].Result.ToString() + "|";
+                }
+                var translatedText = _translationService.ConvertToDictionary(chineseText);
+
+                return Ok(new TranslationResponse
+                {
+                    TranslatedTextContent = translatedText
+                });
             }
-            // 將字串依據 '|' 符號分割
-            string[] parts = englishText.TrimEnd('|').Split('|');
-            // 計算每部分應該包含多少項目
-            //var perPart = Math.Ceiling((decimal)parts.Length / 15);
-            //List<string[]> engTextResult = new List<string[]>();
-            //for (int i = 0; i < perPart; i++)
-            //{
-            //    string[] chunk = parts.Skip(i * 15).Take(15).ToArray();
-            //    engTextResult.Add(chunk);
-            //}
-
-            //Gemini API免費版每分鐘可接收request次數為15次
-            int perPart = (int)Math.Ceiling(parts.Length / 15.0);
-            List<string[]> engTextResult = new List<string[]>();
-            for (int i = 0; i < 15; i++)
-            {
-                string[] chunk = parts.Skip(i * perPart).Take(perPart).ToArray();
-                engTextResult.Add(chunk);
+            catch (Exception ex) {
+                logger.Error(ex.ToString());
+                return StatusCode(500, new { error = "翻譯過程出現錯誤" });
             }
-
-            //var tasks = engTextResult.Select(engTexts => _translationService.TranslateToChineseLocalAsync(engTexts)).ToList();
-            //await Task.WhenAll(tasks);
-            var tasks = engTextResult.Select(engTexts => _translationService.TranslateToChineseAsync(engTexts)).ToList();
-            await Task.WhenAll(tasks);
-
-            string chineseText = "";
-            for (int i = 0; i < tasks.Count; i++) {
-                chineseText += tasks[i].Result.ToString() + "|";
-            }
-            var translatedText = _translationService.ConvertToDictionary(chineseText);
-
-            return Ok(new TranslationResponse
-            {
-                TranslatedTextContent = translatedText
-            });
+            
         }
 
     }
--- TranslationAPI/Program.cs
+++ TranslationAPI/Program.cs
@@ -1,15 +1,22 @@
+using Microsoft.AspNetCore.HttpLogging;
 using NLog;
 using NLog.Config;
+using NLog.Extensions.Logging;
 using NLog.Targets;
 using NLog.Web;
 using TranslationAPI.Interface;
 using WebApplication2.Service;
 using LogLevel = NLog.LogLevel;
 
-NLogConfig.ConfigureLogging();
-var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+var logger = LogManager.Setup().LoadConfigurationFromFile("NLog.config").GetCurrentClassLogger();
+logger.Info("應用程式已啟動");
 
 var builder = WebApplication.CreateBuilder(args);
+
+// 設定 NLog
+builder.Logging.ClearProviders(); // 清除其他日誌提供者
+builder.Host.UseNLog(); // 使用 NLog
+
 // 配置 CORS，讓Chrome擴充功能避免跨域請求被禁止的情形
 builder.Services.AddCors(options =>
 {
@@ -21,16 +28,12 @@ builder.Services.AddCors(options =>
                    .AllowAnyMethod();
         });
 });
-// Add services to the container.
+
 // 加入控制器服務
 builder.Services.AddControllers();
 
 // 配置HttpClient和GeminiTranslationService
 builder.Services.AddHttpClient<IGeminiTranslationService, GeminiTranslationService>();
-
-// 設定 NLog
-builder.Logging.ClearProviders(); // 清除其他日誌提供者
-builder.Host.UseNLog(); // 使用 NLog
 
 var app = builder.Build();
 // 使用 CORS 策略
@@ -57,44 +60,10 @@ app.MapGet("/weatherforecast", () =>
         .ToArray();
     return forecast;
 });
-
-logger.Info("應用程式已啟動");
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
-public static class NLogConfig
-{
-    public static void ConfigureLogging()
-    {
-        var config = new LoggingConfiguration();
-
-        // 檢查專案根目錄下是否存在 "Log" 資料夾，如果不存在則建立
-        string logDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\Log"));
-        if (!Directory.Exists(logDirectory))
-        {
-            Directory.CreateDirectory(logDirectory);
-        }
-        var consoleTarget = new ConsoleTarget("console")
-        {
-            Layout = "${longdate} | ${level:uppercase=true} | ${message} ${exception:format=tostring}"
-        };
-        config.AddTarget(consoleTarget);
-
-        var fileTarget = new FileTarget("file")
-        {
-            FileName = Path.Combine(logDirectory, "myapp-log-${shortdate}.log"),
-            Layout = "${longdate} | ${level:uppercase=true} | ${message} ${exception:format=tostring}"
-        };
-        config.AddTarget(fileTarget);
-
-        config.AddRule(LogLevel.Debug, LogLevel.Fatal, consoleTarget);
-        config.AddRule(LogLevel.Info, LogLevel.Warn, fileTarget);
-
-        LogManager.Configuration = config;
-    }
 }
 
